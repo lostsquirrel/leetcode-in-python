2cc1098f9b708f165ba054fb96cc35c9a404e930
lisong <lisong@cdsunrise.net>
2021-04-10 19:59:43 +0800

    solve problem 15. 3Sum v2

--- 15_3sum.py
+++ 15_3sum.py
@@ -5,20 +5,29 @@ from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         size = len(nums)
-        res = set()
+        res = list()
         if size < 3:
             return res
         nums.sort()
         if nums[0] > 0 or nums[-1] < 0:
             return res
+
         for cursor in range(size):
+            if cursor > 0 and nums[cursor] == nums[cursor - 1]:
+                continue
             left = cursor + 1
             right = size - 1
             while left < right:
+
                 mark = nums[cursor] + nums[left] + nums[right]
                 if mark == 0:
-                    res.add((nums[cursor], nums[left], nums[right]))
+                    res.append([nums[cursor], nums[left], nums[right]])
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
                     right -= 1
+                    left += 1
                 elif mark > 0:
                     right -= 1
                 else:
